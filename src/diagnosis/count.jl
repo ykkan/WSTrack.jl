2ce1fc78bfdaa5f74181c552a595878c714169f8
Yi-Kai Kan <tw.ykkan@gmail.com>
2024-12-19 11:07:28 -0500

    Change API for `covariance`

--- src/diagnosis/count.jl
+++ src/diagnosis/count.jl
@@ -3,7 +3,7 @@ function Base.count(beam::Beam{T}; filter=RectBound{T}()) where {T}
   return mapreduce(x -> filter(x) ? 1 : 0, +, coords)
 end
 
-function Base.count(beam::BeamGPU{T}, filter=RectBound{T}()) where {T}
+function Base.count(beam::BeamGPU{T}; filter=RectBound{T}()) where {T}
   coords = beam.coords
   return mapreduce(x -> filter(x) ? 1 : 0, +, coords)
 end
--- src/diagnosis/covariance.jl
+++ src/diagnosis/covariance.jl
@@ -1,25 +1,25 @@
 export covariance
 
 function covariance(beam::Beam{T}, filter=RectBound{T}()) where {T}
-  npar=beam.npar
   coords = beam.coords
 
   function filtered_cov(x::Coord{T}) where {T} 
     x =  filter(x) ? x : zero(Coord{T})
     return (x * x')
   end
-  cov = mapreduce(filtered_cov, +, coords) / npar
-  return cov
+  n = count(beam; filter=filter)
+  covsum = mapreduce(filtered_cov, +, coords)
+  return (n , covsum/n)
 end
 
 function covariance(beam::BeamGPU{T}, filter=RectBound{T}()) where {T}
-  npar=beam.npar
   coords = beam.coords
 
   function filtered_cov(x::Coord{T}) where {T} 
     x =  filter(x) ? x : zero(Coord{T})
     return (x * x')
   end
-  cov = mapreduce(filtered_cov, +, coords) / npar
-  return cov
+  n = count(beam; filter=filter)
+  covsum = mapreduce(filtered_cov, +, coords)
+  return (n , covsum/n)
 end
